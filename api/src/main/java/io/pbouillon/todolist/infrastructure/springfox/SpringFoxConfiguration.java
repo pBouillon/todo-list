fee5387cd401de4de4be23f1e7df490ac7e1be45
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-10-18 17:31:40 +0200

    Add Swagger and Swagger UI support

---
+++ api/src/main/java/io/pbouillon/todolist/infrastructure/springfox/SpringFoxConfiguration.java
@@ -0,0 +1,49 @@
+package io.pbouillon.todolist.infrastructure.springfox;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+/**
+ * Configuration class to configure the Swagger UI and the Swagger documentation
+ */
+@Configuration
+public class SpringFoxConfiguration {
+
+    /**
+     * Expose controllers and endpoints to be displayed by Swagger
+     * @return The associated Java Bean
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * Specify the Swagger UI information do be displayed
+     * @return The associated Java Bean
+     */
+    private ApiInfo apiInfo() {
+        Contact author = new Contact("Pierre Bouillon", "https://pbouillon.github.io", "PierreBouillon@duck.com");
+
+        return new ApiInfo(
+                "TODO list REST API",
+                "Simple and unambitious TODO list API.",
+                "API TOS",
+                "Terms of service",
+                author,
+                "License of API", "about:blank", Collections.emptyList());
+    }
+
+}
