b3914266f88c8b8b9eed2b822bc0ceafc29df34a
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-10-18 17:32:06 +0200

    Create the TodoItem controller

---
+++ api/src/main/java/io/pbouillon/todolist/presentation/controllers/TodoItemController.java
@@ -0,0 +1,93 @@
+package io.pbouillon.todolist.presentation.controllers;
+
+import io.pbouillon.todolist.domain.entities.TodoItem;
+import io.pbouillon.todolist.infrastructure.persistence.repositories.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * REST controller dedicated to the {@link TodoItem} resources
+ */
+@RestController
+@RequestMapping(
+        path = "/api/todos",
+        produces = MediaType.APPLICATION_JSON_VALUE)
+public class TodoItemController {
+
+    /**
+     * Data access object to interact with the persisted {@link TodoItem} entities
+     */
+    private final TodoItemRepository todoItemRepository;
+
+    /**
+     * Controller's default constructor
+     * @param repository Data access object to interact with the persisted {@link TodoItem} entities
+     */
+    @Autowired
+    public TodoItemController(TodoItemRepository repository) {
+        todoItemRepository = repository;
+    }
+
+    /**
+     * Fetch all {@link TodoItem}s
+     * @return The persisted {@link TodoItem}s
+     */
+    @GetMapping
+    public ResponseEntity<List<TodoItem>> getTodoItems() {
+        List<TodoItem> todoItems = todoItemRepository.findAll();
+
+        return ResponseEntity.ok()
+                .body(todoItems);
+    }
+
+    /**
+     * Fetch a specific {@link TodoItem}
+     * @return The serialized {@link TodoItem}
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoItem> getTodoItem(@PathVariable String id) {
+        TodoItem todoItem = todoItemRepository.findById(id).orElseThrow();
+
+        return ResponseEntity.ok()
+                .body(todoItem);
+    }
+
+    /**
+     * Create a new {@link TodoItem} from the payload
+     * @param todoItem The {@link TodoItem} to create
+     * @return The created resource
+     */
+    @PostMapping
+    public ResponseEntity<TodoItem> post(@RequestBody TodoItem todoItem) {
+        TodoItem created = todoItemRepository.save(todoItem);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+
+        return ResponseEntity.created(location)
+                .body(created);
+    }
+
+    /**
+     * Delete a {@link TodoItem}
+     * @param id The id of the resource to delete
+     * @return No content on success
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeTodoItem(@PathVariable String id) {
+        TodoItem todoItem = todoItemRepository.findById(id).orElseThrow();
+        todoItemRepository.delete(todoItem);
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
