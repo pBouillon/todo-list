dca8144c5a879960c0ce6baa68a5bcb20815f44f
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-10-20 10:27:40 +0200

    Prepare the CQRS service to handle the TodoItem operations

---
+++ api/src/main/java/io/pbouillon/todolist/application/items/TodoItemService.java
@@ -0,0 +1,7 @@
+package io.pbouillon.todolist.application.items;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoItemService implements TodoItemCommandService, TodoItemQueryService {
+}
